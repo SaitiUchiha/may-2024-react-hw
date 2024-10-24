12a1617d1612532e2ecf432e51714a18b496d2dc
SaitiUchiha <vira.brods@gmail.com>
2024-10-24 19:30:19 +0300

    hw3 done + added edit image

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div>
-            <img src='https://unsplash.com/photos/rock-formations-near-calm-body-of-water-D7AB6RNJF_o' alt="storm"/>
+            <img src='https://images.unsplash.com/photo-1561485039-765c8e81686d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="storm"/>
             {/*а чому картинка не грузиться?*/}
         </div>
     );
